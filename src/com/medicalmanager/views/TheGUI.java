4339440056161b2655bb89bdd4ccb5a87230a8a3
EpicCodez <epiccodez@gmail.com>
2012-10-11 00:09:56 -0400

    stuff

--- src/com/medicalmanager/views/TheGUI.java
+++ src/com/medicalmanager/views/TheGUI.java
@@ -25,99 +25,156 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class TheGUI extends JFrame {
 
+	private CardLayout card = new CardLayout(0,0);
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TheGUI frame = new TheGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JButton mainAppButton;
+	private JButton aboutButton;
+	private JButton newPatientButton;
+	private JButton searchButton;
 
 	/**
 	 * Create the frame.
 	 */
 	public TheGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		setBounds(100, 100, 838, 609);
 		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(card);
+		
+		setContentPane(contentPane);
+		
+		placeMenu();
+		makeWelcomePanel();
+		makePatientPanel();
+		actionTime();
+		
+	}
+
+	public void placeMenu(){
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnNewMenu = new JMenu("New menu");
-		menuBar.add(mnNewMenu);
+		JMenu fileMenu = new JMenu("New menu");
+		menuBar.add(fileMenu);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
-		mnNewMenu.add(mntmNewMenuItem);
+		fileMenu.add(mntmNewMenuItem);
 		
-		JMenu mnNewMenu_1 = new JMenu("New menu");
-		menuBar.add(mnNewMenu_1);
+		JMenu helpMenu = new JMenu("New menu");
+		menuBar.add(helpMenu);
 		
 		JMenuItem menuItem = new JMenuItem("New menu item");
-		mnNewMenu_1.add(menuItem);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		helpMenu.add(menuItem);
 		
-		final CardLayout bob = new CardLayout(0,0);
-		contentPane.setLayout(bob);
-		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, "name_0");
+	}
+	
+	public void makePatientPanel(){
+		JPanel patientPanel = new JPanel();
+		contentPane.add(patientPanel, "patientPanel");
 
 		JSplitPane splitPane = new JSplitPane();
 		
-		JToolBar toolBar = new JToolBar();
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
+		JToolBar patientToolBar = new JToolBar();
+		GroupLayout patientLayout = new GroupLayout(patientPanel);
+		patientLayout.setHorizontalGroup(
+			patientLayout.createParallelGroup(Alignment.LEADING)
 				.addComponent(splitPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 812, Short.MAX_VALUE)
-				.addComponent(toolBar, GroupLayout.DEFAULT_SIZE, 812, Short.MAX_VALUE)
+				.addComponent(patientToolBar, GroupLayout.DEFAULT_SIZE, 812, Short.MAX_VALUE)
 		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup()
-					.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+		patientLayout.setVerticalGroup(
+			patientLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, patientLayout.createSequentialGroup()
+					.addComponent(patientToolBar, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(splitPane, GroupLayout.DEFAULT_SIZE, 513, Short.MAX_VALUE))
 		);
 		
-		JButton btnNewPatient = new JButton("New Patient");
-		toolBar.add(btnNewPatient);
+		newPatientButton = new JButton("New Patient");
+
+		patientToolBar.add(newPatientButton);
 		
-		JButton btnSearch = new JButton("Search");
-		toolBar.add(btnSearch);
+		searchButton = new JButton("Search");
+
+		patientToolBar.add(searchButton);
 		
-		JList list = new JList();
-		list.setMinimumSize(new Dimension(200, 0));
-		splitPane.setLeftComponent(list);
+		JList patientList = new JList();
+		patientList.setMinimumSize(new Dimension(200, 0));
+		splitPane.setLeftComponent(patientList);
 		
-		JTextArea textArea = new JTextArea();
-		splitPane.setRightComponent(textArea);
-		panel_1.setLayout(gl_panel_1);
+		JTextArea patientInfoArea = new JTextArea();
+		splitPane.setRightComponent(patientInfoArea);
+		patientPanel.setLayout(patientLayout);
+	}
+	
+	public void makeWelcomePanel(){
+		JPanel welcomePanel = new JPanel();
+		contentPane.add(welcomePanel, "welcomePanel");
+		card.show(contentPane, "welcomePanel");
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, "name_1");
-		bob.show(contentPane, "name_1");
+		JLabel welcomeLabel = new JLabel("Welcome to the application");
 		
-		JLabel lblStuff = new JLabel("Stuff");
-		panel.add(lblStuff);
+		mainAppButton = new JButton("Main App");
+
 		
-		JButton btnClickToContinue = new JButton("Click to continue to good stuff");
-		btnClickToContinue.addActionListener(new ActionListener() {
+		aboutButton = new JButton("About");
+		
+		JLabel createdByLabel = new JLabel("Created By: Ben Vest");
+		GroupLayout welcomeLayout = new GroupLayout(welcomePanel);
+		welcomeLayout.setHorizontalGroup(
+			welcomeLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(welcomeLayout.createSequentialGroup()
+					.addGap(305)
+					.addComponent(mainAppButton)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(aboutButton)
+					.addContainerGap(363, Short.MAX_VALUE))
+				.addGroup(welcomeLayout.createSequentialGroup()
+					.addGap(313)
+					.addComponent(welcomeLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(355))
+				.addGroup(Alignment.TRAILING, welcomeLayout.createSequentialGroup()
+					.addContainerGap(363, Short.MAX_VALUE)
+					.addComponent(createdByLabel)
+					.addGap(346))
+		);
+		welcomeLayout.setVerticalGroup(
+			welcomeLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(welcomeLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(welcomeLabel)
+					.addGap(30)
+					.addGroup(welcomeLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(aboutButton)
+						.addComponent(mainAppButton))
+					.addPreferredGap(ComponentPlacement.RELATED, 437, Short.MAX_VALUE)
+					.addComponent(createdByLabel)
+					.addContainerGap())
+		);
+		welcomePanel.setLayout(welcomeLayout);
+	}
+	
+	public void actionTime(){
+		mainAppButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				bob.next(contentPane);
+				card.next(contentPane);
 			}
 		});
-		panel.add(btnClickToContinue);
+		
+		newPatientButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				NewPatient newUsar = new NewPatient();
+				newUsar.setVisible(true);
+			}
+		});
+		
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Search dialog = new Search();
+				dialog.setVisible(true);
+			}
+		});
 	}
 }
